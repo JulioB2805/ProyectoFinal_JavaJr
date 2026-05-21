d1709fccae57fe08a626940740b5b5d14b54fc70
Julio Báez <julioemmanuelbaezjara@gmail.com>
2026-05-21 16:04:23 -0300

    Add files via upload

---
+++ webJulio/src/main/java/com/snpp/webJulio/WebJulioApplication.java
@@ -0,0 +1,13 @@
+package com.snpp.webJulio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJulioApplication.class, args);
+	}
+
+}
---
+++ webJulio/src/test/java/com/snpp/webJulio/WebJulioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snpp.webJulio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJulioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
